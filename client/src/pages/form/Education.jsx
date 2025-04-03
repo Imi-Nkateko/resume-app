d6df2f8886faba2faa762fd3cc741616a48d5d0a
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-04-04 00:00:02 +0200

    Update Education.jsx

--- client/src/pages/form/Education.jsx
+++ client/src/pages/form/Education.jsx
@@ -60,7 +60,6 @@ const Education = () => {
                     />
                   </div>
 
-                 
                   <div className="md:col-span-5 text-right">
                     <div className="inline-flex items-end">
                       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
